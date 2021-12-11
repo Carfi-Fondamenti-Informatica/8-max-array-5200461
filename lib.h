a4fabf36d5705d3d2f6ad0d0aa6b32bb265e2e56
5200461 <91668775+5200461@users.noreply.github.com>
2021-12-11 15:48:46 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// CREATED BY BUTTITTA ON 11/12/2021
+//
 
+#ifndef UNTITLED15_LIB_H
+#define UNTITLED15_LIB_H
+float nmaggiore(const float numeri[], int dimensione);
+
+#endif //UNTITLED15_LIB_H
